ea229aae03dfefe6e9ce1eba944449032ba6da73
Teruaki Hayashi <thayashi@lss.mes.titech.ac.jp>
2019-04-02 10:48:13 +0900

    Making UART sending program

---
+++ send_uart.py
@@ -0,0 +1,19 @@
+import sys
+import struct
+import serial
+
+path = '/home/pi/python_test/test.txt'
+
+with open(path, 'rb') as f:
+    s = f.read()
+
+port = serial.Serial(
+            port = "/dev/ttyS0", 
+            baudrate=2.5*10^6, 
+            parity = serial.PARITY_NONE,
+            stopbits = serial.STOPBITS_ONE,
+            bytesize = serial.EIGHTBITS,
+            timeout = None
+            )
+
+port.write(s)
