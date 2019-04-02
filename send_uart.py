7087e0765c6242ceb04df961cd5adcb3de044716
Teruaki Hayashi <thayashi@lss.mes.titech.ac.jp>
2019-04-02 11:27:07 +0900

    mis discription at math

--- send_uart.py
+++ send_uart.py
@@ -9,7 +9,7 @@ with open(path, 'rb') as f:
 
 port = serial.Serial(
             port = "/dev/ttyS0", 
-            baudrate=2.5*10^6, 
+            baudrate=2.5*(10**6), 
             parity = serial.PARITY_NONE,
             stopbits = serial.STOPBITS_ONE,
             bytesize = serial.EIGHTBITS,
